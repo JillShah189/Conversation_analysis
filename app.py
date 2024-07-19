102d148cbc07b8030935c42e49d03e54c3ee28a7
Jill Shah <102149980+JillShah189@users.noreply.github.com>
2024-07-19 14:37:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ import streamlit as st
 from conv_graph import show_page1
 from comb_graph import show_page2
 from ans_accepted import show_page3
+from text_analysis import show_page4
 
 # Sidebar navigation
 st.sidebar.title("Navigation")
-page = st.sidebar.radio("Go to", ["Individual Graphs", "Combined Graph", "Answer Accepted/Not Accpected"])
+page = st.sidebar.radio("Go to", ["Individual Graphs", "Combined Graph", "Answer Accepted/Not Accpected", "Text Analysis"])
 
 # Display the selected page
 if page == "Individual Graphs":
@@ -14,3 +15,5 @@ elif page == "Combined Graph":
     show_page2()
 elif page == "Answer Accepted/Not Accpected":
     show_page3()
+elif page == "Text Analysis":
+    show_page4()
